4cf3f6bdc876488eef0b56742c4571451e2ed694
Luukas Pörtfors <lajp@iki.fi>
2025-09-26 15:18:10 +0200

    feat: send heartbeats to status

--- image/pi.nix
+++ image/pi.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   raspberry-pi-nix = {
     board = "bcm2711";
@@ -6,4 +7,25 @@
   };
 
   sdImage.compressImage = false;
+
+  zramSwap.enable = true;
+
+  systemd.services.gatus-heartbeat = {
+    script = ''
+      set -eu
+      ${pkgs.curl}/bin/curl -X POST -H "Authorization: Bearer $TOKEN" "$ENDPOINT"
+    '';
+
+    environment = {
+      ENDPOINT = "https://status.tietokilta.fi/api/v1/endpoints/_guild-room-infoscreen/external?success=true";
+      TOKEN = "VRXibHcAAE0p6R" + "UbPy84f4KrWlwJvtZr";
+    };
+
+    serviceConfig = {
+      DynamicUser = true;
+      User = "gatus-heartbeat";
+      Type = "oneshot";
+    };
+    startAt = "*:0/1";
+  };
 }
